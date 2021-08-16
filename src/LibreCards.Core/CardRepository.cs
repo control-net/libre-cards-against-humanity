ff392d06ae2b56912436b298d3534fb967d77a2a
Thomas Miller <tomalop@users.noreply.github.com>
2021-08-16 20:21:32 +0200

    Started implementing CardRepository with a test
    TDD - CardRepository minimal implementation (that always passes the test).

---
+++ src/LibreCards.Core/CardRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibreCards.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibreCards.Core
+{
+    public class CardRepository : ICardRepository
+    {
+        public IEnumerable<Card> DrawCards(int count = 1)
+        {
+            var result = new List<Card> { new Card() { Id = 0 } };
+            return result;
+        }
+    }
+}
---
+++ src/LibreCards.Tests/CardRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using LibreCards.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibreCards.Tests
+{
+    public class CardRepositoryTests
+    {
+        private readonly CardRepository _cardRepository;
+
+        public CardRepositoryTests()
+        {
+            _cardRepository = new CardRepository();
+        }
+        
+        [Fact]
+        public void CardRepository_GetOneCard()
+        {
+            var cards = _cardRepository.DrawCards();
+            Assert.Single(cards);
+        }
+    }
+}
