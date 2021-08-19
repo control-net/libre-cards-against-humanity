5fdadde2d6f218215d7e83b036f4d535e9110e6b
Thomas Miller <tomalop@users.noreply.github.com>
2021-08-20 00:24:46 +0200

    Implemented ICardRepository with json storage. Only returns Response cards.

--- src/LibreCards.Core/CardRepository.cs
+++ src/LibreCards.Core/CardRepository.cs
@@ -1,16 +1,65 @@
 ﻿using LibreCards.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.Json;
 
 namespace LibreCards.Core
 {
     public class CardRepository : ICardRepository
     {
+        private readonly string _filePath = Path.Combine(
+            Directory.GetParent(Environment.CurrentDirectory)
+            .Parent
+            .Parent
+            .Parent
+            .FullName,
+            @"LibreCards.Core\default-cards.json");
+        private int _responseCardsIndex;
+
         public IEnumerable<Card> DrawCards(int count = 1)
         {
-            var result = new List<Card> { new Card() { Id = 0 } };
-            return result;
+            if (!File.Exists(_filePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            byte[] rawFileContent;
+            try
+            {
+                rawFileContent = File.ReadAllBytes(_filePath);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+
+            JsonFileStructure jsonInterpretation;
+            try
+            {
+                jsonInterpretation = JsonSerializer.Deserialize<JsonFileStructure>(rawFileContent);
+            }
+            catch (JsonException)
+            {
+                throw new JsonException("Cards collection could not be deserialized");
+            }
+
+            return GetResponseCards(jsonInterpretation, count);
+        }
+        private IEnumerable<Card> GetResponseCards(JsonFileStructure jsonFileStructure, int count = 1)
+        {
+            if ((_responseCardsIndex + count) > jsonFileStructure.Responses.Count())
+            {
+                throw new IndexOutOfRangeException("Out of Response Cards");
+            }
+            for (int i = 0; i < count; i++)
+            {
+                var card = new Card { Id = _responseCardsIndex, Text = jsonFileStructure.Responses[_responseCardsIndex] };
+                _responseCardsIndex++;
+                yield return card;
+            }
         }
     }
 }
--- src/LibreCards.Tests/CardRepositoryTests.cs
+++ src/LibreCards.Tests/CardRepositoryTests.cs
@@ -10,18 +10,30 @@ namespace LibreCards.Tests
 {
     public class CardRepositoryTests
     {
-        private readonly CardRepository _cardRepository;
+        private readonly ICardRepository _cardRepository;
 
         public CardRepositoryTests()
         {
             _cardRepository = new CardRepository();
         }
-        
+
         [Fact]
         public void CardRepository_GetOneCard()
         {
             var cards = _cardRepository.DrawCards();
             Assert.Single(cards);
+        }
+        [Fact]
+        public void CardRepository_DrawManyCards()
+        {
+            var cards = _cardRepository.DrawCards(2);
+            Assert.Equal(2, cards.Count());
+        }
+        [Fact]
+        public void CardRepository_DrawCardsOverLimit()
+        {
+            // Why does this not throw? It escapes return GetResponseCards()
+            Assert.Throws<IndexOutOfRangeException>(() => _cardRepository.DrawCards(int.MaxValue));
         }
     }
 }
