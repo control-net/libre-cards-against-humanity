273c9b32bfe843d6f29c8ee749492abd8f898708
petrspelos <petrspelos@users.noreply.github.com>
2021-09-12 17:55:32 +0200

    Blazor: Card Style & the rest of Hub API

--- src/LibreCards.BlazorApp/LocalGameState.cs
+++ src/LibreCards.BlazorApp/LocalGameState.cs
@@ -1,4 +1,5 @@
 ﻿using LibreCards.BlazorApp.Entities;
+using LibreCards.Core.Entities.Client;
 using Microsoft.AspNetCore.SignalR.Client;
 
 namespace LibreCards.BlazorApp;
@@ -11,6 +12,10 @@ public class LocalGameState
     public Guid LocalPlayerId { get; private set; }
 
     public IEnumerable<Guid> Players => _players;
+
+    public IEnumerable<string> Cards { get; private set; }
+
+    public string TemplateCard { get; private set; }
 
     private List<Guid> _players = new();
 
@@ -26,6 +31,30 @@ public class LocalGameState
         connection.On<Guid>("PlayerJoined", OnPlayerJoined);
         connection.On<Guid>("IdAssigned", OnIdAssigned);
         connection.On<List<Guid>>("PlayerList", OnPlayerListReceived);
+        connection.On<GameModel>("GameStarted", OnGameStarted);
+        connection.On<string, int>("UpdateTemplate", OnUpdateTemplate);
+        connection.On<IEnumerable<string>>("UpdateCards", OnUpdateCards);
+    }
+
+    private void OnUpdateCards(IEnumerable<string> cards)
+    {
+        Cards = cards;
+        GameStateChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void OnGameStarted(GameModel game)
+    {
+        // TODO: assign judge
+        LocalPlayerState = PlayerState.Playing;
+        GameStateChanged?.Invoke(this, EventArgs.Empty);
+
+        _ = _connection.SendAsync("GetMyCards", LocalPlayerId);
+    }
+
+    public void OnUpdateTemplate(string template, int numOfSlots)
+    {
+        TemplateCard = template;
+        GameStateChanged?.Invoke(this, EventArgs.Empty);
     }
 
     private void OnPlayerListReceived(List<Guid> ids)
