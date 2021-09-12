eea6b058a2033effd9bcf3df622a281bdd4cce87
petrspelos <petrspelos@users.noreply.github.com>
2021-09-12 14:39:49 +0200

    Add static to WebApp's Program class

--- src/LibreCards.WebApp/Program.cs
+++ src/LibreCards.WebApp/Program.cs
@@ -1,15 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace LibreCards.WebApp
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
