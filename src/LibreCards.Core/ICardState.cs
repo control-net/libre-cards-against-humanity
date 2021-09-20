342bb8ca1b7c8ea139dce85a39751d302b4a198c
petrspelos <petrspelos@users.noreply.github.com>
2021-09-20 23:50:45 +0200

    Minor Whitespace fix

--- src/LibreCards.Core/ICardState.cs
+++ src/LibreCards.Core/ICardState.cs
@@ -6,7 +6,6 @@ namespace LibreCards.Core
     public interface ICardState
     {
         Template CurrentTemplateCard { get; }
-
         void DrawTemplateCard();
         void RefillPlayerCards(IReadOnlyCollection<Player> players);
     }
